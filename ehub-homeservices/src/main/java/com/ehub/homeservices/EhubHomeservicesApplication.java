48fdaa4eba1bf61aee0f632e42bb41ea3c0cd716
Admin <Admin@192.168.2.101>
2020-06-11 15:10:02 -0400

    Commit 1

---
+++ ehub-homeservices/src/main/java/com/ehub/homeservices/EhubHomeservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ehub.homeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhubHomeservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhubHomeservicesApplication.class, args);
+	}
+
+}
